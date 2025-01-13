84dac48a1752ba68ad7966c970027320148a6987
Salih Akbas <salihhakbass@gmail.com>
2025-01-14 02:45:44 +0300

    Added forecastData value, fetchWeather and showError functions

--- app/src/main/java/com/salihakbas/weatherappcompose/ui/home/HomeContract.kt
+++ app/src/main/java/com/salihakbas/weatherappcompose/ui/home/HomeContract.kt
@@ -1,14 +1,21 @@
 package com.salihakbas.weatherappcompose.ui.home
 
-import com.salihakbas.weatherappcompose.data.model.Weather
+import com.salihakbas.weatherappcompose.data.model.CurrentResponse
+import com.salihakbas.weatherappcompose.data.model.ForecastResponse
 
 object HomeContract {
     data class UiState(
         val isLoading: Boolean = false,
-        val weatherData: Weather? = null
+        val weatherData: CurrentResponse? = null,
+        val forecastData: ForecastResponse? = null
     )
 
-    sealed class UiAction
+    sealed class UiAction {
+        data class FetchWeather(val lat: Double, val lon: Double, val apiKey: String) : UiAction()
+    }
 
-    sealed class UiEffect
+    sealed class UiEffect {
+        data class ShowError(val message: String) : UiEffect()
+
+    }
 }
