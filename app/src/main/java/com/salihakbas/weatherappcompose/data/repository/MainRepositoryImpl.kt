bd68ce50128ff0319325fa47e99689a033e0dd3a
Salih Akbas <salihhakbass@gmail.com>
2025-01-14 02:44:12 +0300

    Added forecastWeather function

--- app/src/main/java/com/salihakbas/weatherappcompose/data/repository/MainRepositoryImpl.kt
+++ app/src/main/java/com/salihakbas/weatherappcompose/data/repository/MainRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.salihakbas.weatherappcompose.data.repository
 
-import com.salihakbas.weatherappcompose.data.model.Weather
+import com.salihakbas.weatherappcompose.data.model.CurrentResponse
+import com.salihakbas.weatherappcompose.data.model.ForecastResponse
 import com.salihakbas.weatherappcompose.data.source.remote.MainService
 import com.salihakbas.weatherappcompose.domain.repository.MainRepository
 import javax.inject.Inject
@@ -8,7 +9,16 @@ import javax.inject.Inject
 class MainRepositoryImpl @Inject constructor(
     private val mainService: MainService,
 ) : MainRepository {
-    override suspend fun getWeather(lat: Double, lon: Double, apiKey: String): Weather {
-        return mainService.getWeather(lat,lon,apiKey)
+    override suspend fun getWeather(lat: Double, lon: Double, apiKey: String): CurrentResponse {
+        return mainService.getWeather(lat, lon, apiKey)
     }
+
+    override suspend fun getForecastWeather(
+        lat: Double,
+        lon: Double,
+        apiKey: String
+    ): ForecastResponse {
+        return mainService.getForecastWeather(lat, lon, apiKey)
+    }
+
 }
--- app/src/main/java/com/salihakbas/weatherappcompose/domain/repository/MainRepository.kt
+++ app/src/main/java/com/salihakbas/weatherappcompose/domain/repository/MainRepository.kt
@@ -1,7 +1,10 @@
 package com.salihakbas.weatherappcompose.domain.repository
 
-import com.salihakbas.weatherappcompose.data.model.Weather
+import com.salihakbas.weatherappcompose.data.model.CurrentResponse
+import com.salihakbas.weatherappcompose.data.model.ForecastResponse
 
 interface MainRepository {
-    suspend fun getWeather(lat: Double, lon: Double, apiKey: String): Weather
+    suspend fun getWeather(lat: Double, lon: Double, apiKey: String): CurrentResponse
+
+    suspend fun getForecastWeather(lat:Double,lon: Double,apiKey: String) : ForecastResponse
 }
