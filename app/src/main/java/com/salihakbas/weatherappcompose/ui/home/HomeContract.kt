67b9fb85c649c5dc2c130affcf5c6316ee168082
Salih Akbas <salihhakbass@gmail.com>
2025-01-15 19:47:24 +0300

    Added forecast list

--- app/src/main/java/com/salihakbas/weatherappcompose/ui/home/HomeContract.kt
+++ app/src/main/java/com/salihakbas/weatherappcompose/ui/home/HomeContract.kt
@@ -1,20 +1,22 @@
 package com.salihakbas.weatherappcompose.ui.home
 
-import com.salihakbas.weatherappcompose.data.model.CurrentResponse
-import com.salihakbas.weatherappcompose.data.model.ForecastResponse
+import com.salihakbas.weatherappcompose.data.model.Weather
+import com.salihakbas.weatherappcompose.data.model.WeatherResponse
 
 object HomeContract {
     data class UiState(
         val isLoading: Boolean = false,
-        val weatherData: CurrentResponse? = null,
-        val forecastData: ForecastResponse? = null
+        val weatherData: WeatherResponse? = null,
+        val forecastList: List<WeatherResponse> = emptyList()
     )
 
     sealed class UiAction {
-        data class FetchWeather(val lat: Double, val lon: Double, val apiKey: String) : UiAction()
+        data class FetchWeather(val city: String, val apiKey: String) : UiAction()
+        object NavigateToSearch : UiAction()
     }
 
     sealed class UiEffect {
+        data object NavigateToSearch : UiEffect()
         data class ShowError(val message: String) : UiEffect()
 
     }
